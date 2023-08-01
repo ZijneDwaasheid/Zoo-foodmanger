38ff4b6b78fb13feb27e1a387d15c4a361a889bb
Arnie <arne@inuits.eu>
2023-08-01 15:02:40 +0200

    wat gecomment

--- frontend/app/components/caretaker/remove-animal.js
+++ frontend/app/components/caretaker/remove-animal.js
@@ -6,24 +6,23 @@ import { action } from '@ember/object';
 
 export default class RemoveAnimalComponent extends Component {
     @service store;
+    
   
     @action
     async deleteAnimal(caretaker, animal) {
 
-        let caretakerWithAnimalToDelete = await this.store.peekRecord('caretaker', caretaker.id); //doing this because that's how they do it in the ember data example
-        let animalToDelete = await this.store.peekRecord('animal', animal.id);
-
-        let caretakerAnimals = await caretakerWithAnimalToDelete.animals;
+        let caretakerAnimals = await caretaker.animals;
+        let animalToDelete = await animal;
 
         //some logs to understand the problem, at the moment I dont understand :(
-        console.log('name caretaker: ',caretaker.name);
-        console.log('species of the animal to delete: ',animalToDelete.species);
-        console.log('what is in animalToDelete: ',animalToDelete);
-        console.log('what is caretaker animals: ',caretakerAnimals);
+        console.log('name caretaker: ',caretaker.name); //this gives the right name
+        console.log('species of the animal to delete: ',animalToDelete.species); //this gives the right species
+        console.log('what is in animalToDelete: ',animalToDelete); // this gives something I don't understand
+        console.log('what is in caretaker animals: ',caretakerAnimals); // this also, but somewhere, you van see the animals that are linked with the caretaker
         console.log('type of animalToDelete: ', typeof(animalToDelete));
-        console.log('type of caretakerAnimals ',typeof(caretakerAnimals))
+        console.log('type of caretakerAnimals ',typeof(caretakerAnimals));
 
-        caretakerAnimals.removeObject(animalToDelete); //here is a type error :(
+        caretakerAnimals.removeObject(animalToDelete); //here is a typeError :(
         await caretaker.save();
     }
 } 
