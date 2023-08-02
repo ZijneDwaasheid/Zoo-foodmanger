9e4ad7466d8c13d3d7a5b0a899286b97ee3365fc
Arnie <arne@inuits.eu>
2023-08-02 09:02:35 +0200

    removing relation

--- frontend/app/components/caretaker/remove-animal.js
+++ frontend/app/components/caretaker/remove-animal.js
@@ -7,18 +7,13 @@ export default class RemoveAnimalComponent extends Component {
 
   @action
   async deleteAnimal(caretaker, animal) {
-    let caretakerAnimals = await caretaker.animals;
+    let caretakerAnimals = [ ... await caretaker.animals];
     let animalToDelete = await animal;
 
-    //some logs to understand the problem, at the moment I dont understand :(
-    console.log('name caretaker: ', caretaker.name); //this gives the right name
-    console.log('species of the animal to delete: ', animalToDelete.species); //this gives the right species
-    console.log('what is in animalToDelete: ', animalToDelete); // this gives something I don't understand
-    console.log('what is in caretaker animals: ', caretakerAnimals); // this also, but somewhere, you van see the animals that are linked with the caretaker
-    console.log('type of animalToDelete: ', typeof animalToDelete);
-    console.log('type of caretakerAnimals ', typeof caretakerAnimals);
+    caretaker.animals = caretakerAnimals.filter((animal)=>{
+      return animal.id !== animalToDelete.id;
+    });
 
-    caretakerAnimals.removeObject(animalToDelete); //here is a typeError :(
     await caretaker.save();
   }
 }
